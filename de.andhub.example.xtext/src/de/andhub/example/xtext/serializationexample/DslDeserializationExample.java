db911ecaaef234b678462a2558282631e287b173
Andreas Huber <andreas@andhub.de>
2020-12-10 17:06:41 +0100

    Initialize package prior to deserialization of model specified in dsl

--- de.andhub.example.xtext/src/de/andhub/example/xtext/serializationexample/DslDeserializationExample.java
+++ de.andhub.example.xtext/src/de/andhub/example/xtext/serializationexample/DslDeserializationExample.java
@@ -7,44 +7,21 @@ import org.eclipse.xtext.resource.XtextResourceSet;
 
 import com.google.inject.Injector;
 
+import de.andhub.example.emf.todo.TodoPackage;
 import de.andhub.example.emf.todo.TodoSystem;
 import de.andhub.example.xtext.TodoDslStandaloneSetup;
 
 public class DslDeserializationExample {
 
 	public static void main(String[] args) {
-//		URI uri = URI.createURI("resources/serializedModelInstanceGeneratedWithEditor.todo");
-//		ResourceSet resourceSet = null;
-//		Resource resource = null;
-//		TodoFactory.eINSTANCE.createTodoSystem();
-//		TodoSystem model = TodoFactory.eINSTANCE.createTodoSystem();
-//		try {
-//			resourceSet = new ResourceSetImpl();
-//			resource = resourceSet.createResource(uri);
-//			EList<EObject> contents = resource.getContents();
-//			resource.load(stream, Collections.EMPTY_MAP);
-//			if (contents != null && !contents.isEmpty()) {
-//				model = (TodoSystem) contents.get(0);
-//				// model.setResource(resource);
-//			}
-//
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		} finally {
-//			try {
-//				stream.close();
-//			} catch (IOException e) {
-//				e.printStackTrace();
-//			}
-//		}
-		
+		TodoPackage.eINSTANCE.eClass();
 		new org.eclipse.emf.mwe.utils.StandaloneSetup().setPlatformUri("../");
 		Injector injector = new TodoDslStandaloneSetup().createInjectorAndDoEMFRegistration();
 		XtextResourceSet resourceSet = injector.getInstance(XtextResourceSet.class);
 		resourceSet.addLoadOption(XtextResource.OPTION_RESOLVE_ALL, Boolean.TRUE);
 		Resource resource = resourceSet.getResource(URI.createURI("resources/serializedDslFile.tododsl"), true);
 		TodoSystem model = (TodoSystem) resource.getContents().get(0);
+		System.out.println(model);
 		
 	}
 }
